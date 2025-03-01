08bd284e6a1b65d161e1f742e47b7dba29d044b8
afroja akter <afrojaakter10bd@gmail.com>
2025-03-01 13:09:53 +0600

    added html page

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ document.getElementById('date').textContent = date;
 
 // clicked card button
 const buttons = document.querySelectorAll(".card-btn");
-const taskplus = parseInt(document.getElementById('task-plas').textContent);
-const taskMinus = parseInt(document.getElementById('task-minus').textContent) ;
 
 taskplusValue = 23;
 taskMinusValue = 5;
@@ -33,7 +31,55 @@ for(let button of buttons){
             button.disabled = true;
             button.style.background = 'gray'
         }
-       
+
+
+        let time = new Date();
+        let hours = time.getHours();
+        let nowHours = hours < 10 ? '0'+ hours : hours;
+        let minute = time.getMinutes();
+        let nowMinute = minute < 10 ? '0' + minute : minute;
+        let pmam = hours > 12 ? 'PM' : "AM" ;
+        
+        const nowTime = nowHours + ':' + nowMinute + pmam; 
+
+        let taskCard = button.closest('.task'); 
+        let titleElement = taskCard.querySelector('.card-title');
+        let title = titleElement.textContent;
+        let p = document.createElement('p');
+        p.classList.add('py-5')
+        p.innerText = 'You have completed the task ' + title + nowTime;
+
+        const div = document.getElementById('task-history');
+        div.appendChild(p);
 
     })
 }
+
+// clear buttons
+document.getElementById('clear-btn').addEventListener('click' , function(){
+    document.getElementById('task-history').textContent = '';
+})
+
+// theme color changed
+const colors = ['#ccd1d1', '#a2d9ce', '#fad7a0', '#aeb6bf', '#9FE2BF' , '#f5cba7'];
+let randomColor = 0;
+document.getElementById('theme').addEventListener('click', function(){
+    
+        if(randomColor < colors.length){
+            document.getElementById('body').style.background = colors[randomColor];
+            randomColor++;
+        }
+        else{
+            randomColor = 0;
+        }
+        
+        
+});
+
+
+// page button clicked
+document.getElementById('page-btn').addEventListener('click', function(){
+    window.location.href = 'question.html';
+})
+
+alert('hello')
